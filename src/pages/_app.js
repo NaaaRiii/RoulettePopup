ec37f654fdefcdc5f015093f9664c2cd42a5df2e
NaaaRiii <axenarikilo48698062@gmail.com>
2024-12-12 12:01:49 +0900

    fin fix_for_amplify_6

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -7,7 +7,7 @@ import '../components/styles.css';
 import { Authenticator } from '@aws-amplify/ui-react';
 import { Amplify } from 'aws-amplify';
 import '@aws-amplify/ui-react/styles.css';
-import outputs from "../../amplify_outputs.json";
+import outputs from '../../amplify_outputs.json';
 
 Amplify.configure(outputs);
 
