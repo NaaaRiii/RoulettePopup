602c3e0a87a4fbd3938494190fb45655cd2ca361
NaaaRiii <axenarikilo48698062@gmail.com>
2025-03-19 17:26:49 +0900

    fin fix_code0319

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@
 const nextConfig = {
 
   env: {
-    NEXT_PUBLIC_RAILS_API_URL: 'https://rails-alb-284067406.ap-northeast-1.elb.amazonaws.com',
+    NEXT_PUBLIC_RAILS_API_URL: 'https://rails-alb-636808077.ap-northeast-1.elb.amazonaws.com',
   },
 
   webpack: (config, { isServer }) => {
