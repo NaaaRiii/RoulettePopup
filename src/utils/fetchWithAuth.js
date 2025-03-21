3f85bef22784af9af158f47f5a57280a52c2fede
NaaaRiii <axenarikilo48698062@gmail.com>
2025-03-21 14:53:12 +0900

    fin fix_code032102

--- src/utils/fetchWithAuth.js
+++ src/utils/fetchWithAuth.js
@@ -1,30 +1,30 @@
-import { Auth } from '@aws-amplify/auth';
-//import { Auth } from 'aws-amplify';
-//import outputs from '../../amplify_outputs.json';
+//import { Auth } from '@aws-amplify/auth';
+////import { Auth } from 'aws-amplify';
+////import outputs from '../../amplify_outputs.json';
 
-//Amplify.configure(outputs);
+////Amplify.configure(outputs);
 
-export const fetchWithAuth = async (url, options = {}) => {
-  try {
-    const user = await Auth.currentAuthenticatedUser();
-    const token = user.signInUserSession.idToken.jwtToken;
+//export const fetchWithAuth = async (url, options = {}) => {
+//  try {
+//    const user = await Auth.currentAuthenticatedUser();
+//    const token = user.signInUserSession.idToken.jwtToken;
 
-    // CognitoのIDトークンをAuthorizationヘッダーに付与
-    const defaultHeaders = {
-      'Content-Type': 'application/json',
-      Authorization: `Bearer ${token}`,
-    };
+//    // CognitoのIDトークンをAuthorizationヘッダーに付与
+//    const defaultHeaders = {
+//      'Content-Type': 'application/json',
+//      Authorization: `Bearer ${token}`,
+//    };
 
-    return await fetch(url, {
-      ...options,
-      credentials: 'include',
-      headers: {
-        ...defaultHeaders,
-        ...(options.headers || {}),
-      },
-    });
-  } catch (error) {
-    console.error('Error in fetchWithAuth:', error);
-    throw error;
-  }
-};
+//    return await fetch(url, {
+//      ...options,
+//      credentials: 'include',
+//      headers: {
+//        ...defaultHeaders,
+//        ...(options.headers || {}),
+//      },
+//    });
+//  } catch (error) {
+//    console.error('Error in fetchWithAuth:', error);
+//    throw error;
+//  }
+//};
