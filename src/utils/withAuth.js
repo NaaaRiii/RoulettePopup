de07ee4a738df71b0ed4a36b2ba2509974ced39f
NaaaRiii <axenarikilo48698062@gmail.com>
2025-03-21 17:30:26 +0900

    fin fix_code032106

--- src/utils/withAuth.js
+++ src/utils/withAuth.js
@@ -1,48 +1,48 @@
-import React from 'react';
-import { useEffect, useState } from 'react';
-import { useRouter } from 'next/router';
+//import React from 'react';
+//import { useEffect, useState } from 'react';
+//import { useRouter } from 'next/router';
 
-const withAuth = (WrappedComponent) => {
-  const ComponentWithAuth = (props) => {
-    const [loading, setLoading] = useState(true);
-    const router = useRouter();
+//const withAuth = (WrappedComponent) => {
+//  const ComponentWithAuth = (props) => {
+//    const [loading, setLoading] = useState(true);
+//    const router = useRouter();
 
-    useEffect(() => {
-      const checkLoginStatus = async () => {
-        try {
-          const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/check_login`, {
-            method: 'GET',
-            credentials: 'include'
-          });
-          const data = await response.json();
+//    useEffect(() => {
+//      const checkLoginStatus = async () => {
+//        try {
+//          const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/check_login`, {
+//            method: 'GET',
+//            credentials: 'include'
+//          });
+//          const data = await response.json();
 
-          if (!data.logged_in) {
-            router.push({
-              pathname: '/login',
-              query: { message: 'ログインをしてください' }
-            });
-          } else {
-            setLoading(false);
-          }
-        } catch (error) {
-          console.error('Error checking login status:', error);
-          router.push('/');
-        }
-      };
+//          if (!data.logged_in) {
+//            router.push({
+//              pathname: '/login',
+//              query: { message: 'ログインをしてください' }
+//            });
+//          } else {
+//            setLoading(false);
+//          }
+//        } catch (error) {
+//          console.error('Error checking login status:', error);
+//          router.push('/');
+//        }
+//      };
 
-      checkLoginStatus();
-    }, [router]);
+//      checkLoginStatus();
+//    }, [router]);
 
-    if (loading) {
-      return <p>Loading...</p>; // ローディング中のUIを表示
-    }
+//    if (loading) {
+//      return <p>Loading...</p>; // ローディング中のUIを表示
+//    }
 
-    return <WrappedComponent {...props} />;
-  };
+//    return <WrappedComponent {...props} />;
+//  };
 
-  ComponentWithAuth.displayName = `withAuth(${WrappedComponent.displayName || WrappedComponent.name || 'Component'})`;
+//  ComponentWithAuth.displayName = `withAuth(${WrappedComponent.displayName || WrappedComponent.name || 'Component'})`;
 
-  return ComponentWithAuth;
-};
+//  return ComponentWithAuth;
+//};
 
-export default withAuth;
+//export default withAuth;
