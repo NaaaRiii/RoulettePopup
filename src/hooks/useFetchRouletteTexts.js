a07c9f761bc88936ecfec1d3b7517e065dabae74
NaaaRiii <axenarikilo48698062@gmail.com>
2025-04-07 16:24:21 +0900

    fin fix_code0407

---
+++ src/hooks/useFetchRouletteTexts.js
@@ -0,0 +1,33 @@
+import { useState, useEffect } from 'react';
+
+//import { fetchWithAuth } from '../utils/fetchWithAuth';
+
+export const useFetchRouletteTexts = () => {
+  const [rouletteTexts, setRouletteTexts] = useState([]);
+
+  useEffect(() => {
+    let isMounted = true;
+
+    const fetchAllRouletteTexts = async () => {
+      try {
+        const response = await await fetch(`${process.env.NEXT_PUBLIC_RAILS_API_URL}/api/roulette_texts`,
+          { method: 'GET' }
+        );
+        const data = await response.json();
+        if (isMounted) {
+          setRouletteTexts(data);
+        }
+      } catch (error) {
+        console.error('Error fetching roulette texts:', error);
+      }
+    };
+
+    fetchAllRouletteTexts();
+
+    return () => {
+      isMounted = false;
+    };
+  }, []);
+
+  return { rouletteTexts, setRouletteTexts };
+};
