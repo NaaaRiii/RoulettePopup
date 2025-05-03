7ebc19feabac61024efd938ecca22e0f3d02b2b4
NaaaRiii <axenarikilo48698062@gmail.com>
2025-05-03 14:54:41 +0900

    fin fix_code0502

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -3,7 +3,7 @@ import { GoalsProvider } from '../contexts/GoalsContext';
 import { TicketsProvider } from '../contexts/TicketsContext';
 import '../components/styles.css';
 
-import { Authenticator } from '@aws-amplify/ui-react';
+//import { Authenticator } from '@aws-amplify/ui-react';
 import '@aws-amplify/ui-react/styles.css';
 import { Amplify } from 'aws-amplify';
 import outputs from '/amplify_outputs.json';
@@ -13,13 +13,13 @@ function MyApp({ Component, pageProps }) {
   Amplify.configure(outputs);
 
   return (
-    <Authenticator>
+    //<Authenticator>
       <GoalsProvider>
         <TicketsProvider>
           <Component {...pageProps} />
         </TicketsProvider>
       </GoalsProvider>
-    </Authenticator>
+    //</Authenticator>
   );
 }
 
