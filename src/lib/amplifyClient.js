5530209c0eb0c4559c2c683b0b2cca574ff58bfe
NaaaRiii <axenarikilo48698062@gmail.com>
2025-05-08 16:02:42 +0900

    fin fix_code0508

--- src/lib/amplifyClient.js
+++ src/lib/amplifyClient.js
@@ -1,5 +1,5 @@
 import { Amplify } from 'aws-amplify';
-import outputs from '../amplify_outputs.json';
+import outputs from '/amplify_outputs.json';
 
 // 一度だけ、モジュールロード時に初期化
 Amplify.configure(outputs);
