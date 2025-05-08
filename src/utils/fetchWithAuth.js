19e01ad582446cbdc271f4d6e2dfdb33a4cc5115
NaaaRiii <axenarikilo48698062@gmail.com>
2025-05-08 15:49:39 +0900

    fin fix_code0507

--- src/utils/fetchWithAuth.js
+++ src/utils/fetchWithAuth.js
@@ -1,16 +1,21 @@
+import '../lib/amplifyClient.js'; 
 import { Auth } from 'aws-amplify';
+
 
 export async function fetchWithAuth(path, options = {}) {
   const session = await Auth.currentSession();
   const token = session.getIdToken().getJwtToken();
 
-  return fetch(`${process.env.NEXT_PUBLIC_RAILS_API_URL}${path}`, {
-    credentials: 'include',
-    headers: {
-      'Content-Type': 'application/json',
-      Authorization: `Bearer ${token}`,
-      ...options.headers,
-    },
-    ...options,
-  });
+  return fetch(
+    `${process.env.NEXT_PUBLIC_RAILS_API_URL}${path}`,
+    {
+      credentials: 'include',
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: `Bearer ${token}`,
+        ...options.headers,
+      },
+      ...options,
+    }
+  );
 }
