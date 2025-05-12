95006d94b23cae7611772163e8f46cedbacdb540
NaaaRiii <axenarikilo48698062@gmail.com>
2025-05-12 22:33:32 +0900

    fin fix_code051215

--- src/lib/amplifyClient.js
+++ src/lib/amplifyClient.js
@@ -1,4 +1,4 @@
-import { Amplify } from 'aws-amplify';
+import { Amplify, Auth } from 'aws-amplify';
 import awsExports from '../amplify_outputs.json';
 
 Amplify.configure({
@@ -8,12 +8,12 @@ Amplify.configure({
       {
         name: 'plusOneApi',
         endpoint: process.env.NEXT_PUBLIC_RAILS_API_URL,
-        region: awsExports.aws_cognito_region,              // 例: 'ap-northeast-1'
-        custom_header: async () => {
-          // もし API 認証を UserPool でやるなら、ヘッダーに Authorization を付与
-          const session = await Amplify.Auth.currentSession();
-          return { Authorization: session.getIdToken().getJwtToken() };
-        }
+        region: awsExports.aws_cognito_region,
+        custom_header: async () => ({
+          Authorization: (await Auth.currentSession())
+            .getIdToken()
+            .getJwtToken()
+        })
       }
     ]
   }
