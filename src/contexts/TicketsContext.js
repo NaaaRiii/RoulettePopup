8cd464d48e0c1952ff4d4b5e3eeed8b6415e59fe
NaaaRiii <axenarikilo48698062@gmail.com>
2025-06-04 16:17:27 +0900

    fin fix_TicketsContext5

--- src/contexts/TicketsContext.js
+++ src/contexts/TicketsContext.js
@@ -1,4 +1,4 @@
-import React, { createContext, useState, useEffect } from 'react';
+import React, { createContext, useState, useEffect, useCallback } from 'react';
 
 import { fetchWithAuth } from '../utils/fetchWithAuth';
 
